1fc4a057e5e534c4d5b1217a6bab05279699526e
Gustavo Montamat <gmontamat@gmail.com>
2024-11-19 10:27:45 -0500

    Add screenshot capture script

---
+++ src/screenshot/screenshot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Define output directory
+OUTPUT_DIR=${OUTPUT_DIR:-data/screenshots}
+# Create the directory if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+# Get the interval from the first argument or 5 seconds
+interval=${1:-5}
+
+take_screenshot() {
+    TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+    FILENAME="$OUTPUT_DIR/screenshot_$TIMESTAMP.png"
+
+    if [ "$XDG_SESSION_DESKTOP" = "KDE" ]; then
+        # For KDE Plasma using Spectacle
+        spectacle -b -a -n -o "$FILENAME"
+    elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+        # For X11 using xdotool and import
+        WINDOW_ID=$(xdotool getactivewindow)
+        import -window "$WINDOW_ID" "$FILENAME"
+    elif [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+        # For Wayland, assuming grim is installed
+        grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused==true).rect | "(.x),(.y) (.width)x(.height)"')" "$FILENAME"
+    else
+        echo "Unsupported session type or desktop environment."
+    fi
+}
+
+while true; do
+  take_screenshot
+  sleep "$interval"
+done
