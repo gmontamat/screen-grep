3318c2d9024a9d456db0661b86dd9811015adb7d
Gustavo Montamat <gmontamat@gmail.com>
2024-11-20 18:16:40 -0500

    Fix image display issues using static dir

--- src/search/app.py
+++ src/search/app.py
@@ -1,13 +1,27 @@
 import os
+import shutil
 
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 
+# Get directory for images
+IMAGES_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static", "images")
+os.makedirs(IMAGES_PATH, exist_ok=True)
 # Initialize Elasticsearch client
 ES_HOST = os.getenv("ES_HOST", "localhost")
 es = Elasticsearch([f"http://{ES_HOST}:9200"])
+
+
+def move_images(image_path):
+    """
+    Move an image to the IMAGES_PATH directory if it does not
+    already exist there.
+    """
+    filename = os.path.basename(image_path)
+    if not os.path.isfile(os.path.join(IMAGES_PATH, filename)):
+        shutil.move(image_path, IMAGES_PATH)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -29,6 +43,9 @@ def search():
                 }
             )
             ocr_results = ocr_response["hits"]["hits"]
+            for result in ocr_results:
+                move_images(result["_source"]["image_path"])
+                result["_source"]["image_path"] = os.path.basename(result["_source"]["image_path"])
 
             # Search in Elasticsearch for Caption matches
             caption_response = es.search(
@@ -42,15 +59,19 @@ def search():
                 }
             )
             caption_results = caption_response["hits"]["hits"]
+            for result in caption_results:
+                move_images(result["_source"]["image_path"])
+                result["_source"]["image_path"] = os.path.basename(result["_source"]["image_path"])
     return render_template("search_results.html", ocr_results=ocr_results, caption_results=caption_results)
 
 
-@app.route("/image/<id>")
-def show_image(_id):
+@app.route("/display/<screenshot_id>")
+def display(screenshot_id):
     # Retrieve the document by ID
-    response = es.get(index="screenshots", id=_id)
+    response = es.get(index="screenshots", id=screenshot_id)
     result = response["_source"]
-    return render_template("full_image.html", result=result)
+    result["image_path"] = os.path.basename(result["image_path"])
+    return render_template("display.html", result=result)
 
 
 if __name__ == "__main__":
