d34611d6bfef9f7c6d29fb0a9eafd9d97fe83868
Gustavo Montamat <gmontamat@gmail.com>
2024-11-21 21:57:08 -0500

    Rename image storage

--- src/caption/main.py
+++ src/caption/main.py
@@ -10,7 +10,7 @@ IMAGE2TEXT_MODEL = FlorenceCaption()
 OCR_MODEL = TesseractOCR()
 ES_HOST = os.getenv("ES_HOST", "localhost")
 SCREENSHOTS = "data/screenshots"
-PROCESSED = "data/processed"
+STORAGE = "data/storage"
 
 
 def caption_images(input_path: str, output_path: str, es: Elasticsearch):
@@ -30,9 +30,9 @@ def caption_images(input_path: str, output_path: str, es: Elasticsearch):
 
 if __name__ == "__main__":
     subprocess.run([f"{os.path.dirname(__file__)}/init-index.sh"])
-    db = Elasticsearch([f"http://{ES_HOST}:9200"])
-    if not os.path.exists(PROCESSED):
-        os.makedirs(PROCESSED, exist_ok=True)
+    elastic = Elasticsearch([f"http://{ES_HOST}:9200"])
+    if not os.path.exists(STORAGE):
+        os.makedirs(STORAGE, exist_ok=True)
     while True:
-        caption_images(SCREENSHOTS, PROCESSED, db)
+        caption_images(SCREENSHOTS, STORAGE, elastic)
         time.sleep(300)  # Sleep for 5 minutes (300 seconds) before the next call
